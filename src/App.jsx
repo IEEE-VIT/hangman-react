a2f3e52a5a40ed8abe74d6805bd7ca35fa74771c
Aneesh Panda <aneeshpanda.ap@gmail.com>
2021-10-01 01:16:13 +0530

    init: project

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import "./App.css";
 
-function App() {
-  return <div className="App">Hangman</div>;
-}
+const App = () => {
+  /* Import Words from helpers, use a Math.random to get a word */
+  return (
+    <div className="App">
+      Hangman{" "}
+      {/* Maintain a state for correct letters, wrong letters, remaining letters. For this, the letters component and the hangman component along with guessing should be imported. Import Guessing as well, to know what letters are correct. 
+      Wrong letters is passed as a prop to HangmanFigure Component. */}{" "}
+    </div>
+  );
+};
 
 export default App;
---
+++ src/components/letter/Letter.component.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+
+const Letter = () => {
+  const [clicked, setClicked] = useState(false);
+  /*Get the value of the alphabet as props from Letters component and add Letter component*/
+  /*create a handleonClick function that changes the state to true.*/
+  return (
+    <div>
+      {/*Value of the alphabet, change the state when the letter is clicked*/}
+    </div>
+  );
+};
+
+export default Letter;
