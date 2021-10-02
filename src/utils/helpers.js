a19d94bb166e78d2c475531817684d0ad4cbfaf0
sreechand2002 <sreechand2002@gmail.com>
2021-10-02 19:32:53 +0530

    resolved

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,10 +1,10 @@
 export const checkWinner = (correct, wrong, word) => {
-  /*Define condition for win and loop it.*/
-  /*Define condition for lose*/
+  /* Define condition for win and loop it. */
+  /* Define condition for lose */
 };
 
 export const alphabets = () => {
-  /*Get alphabets of the English language*/
+  /* Get alphabets of the English language */
 };
 
 export const words = () => {
@@ -18,5 +18,5 @@ export const words = () => {
     "VIT",
   ];
 
-  /*Import this in Guessing component and use it.*/
+  /* Import this in Guessing component and use it. */
 };
