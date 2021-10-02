08b3cfd7bfa879b22fb0378e3947233dce8767a9
Muhammad Tanveer ul Haq <43910948+MuhammadTanveer94@users.noreply.github.com>
2021-10-03 00:01:36 +0500

    Add theme support (#24)
    Co-authored-by: tanveer_ulhaq <tanveer.ulhaq@uniquesoftwaredev.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./App.css";
-// import { words } from "./utils/helpers";
+import { words } from "./utils/helpers";
+import { ThemeContext } from "./context/theme-context";
+
 
 // const getRandomWord = () => {
 //   const wordsPool = words();
@@ -8,11 +10,33 @@ import "./App.css";
 // };
 
 const App = () => {
-  // const [currentWord] = React.useState(getRandomWord());
+  const [currentWord] = React.useState(getRandomWord());
+  const { theme, toggle, dark } = React.useContext(ThemeContext);
 
   return (
-    <div className="App">
-      Hangman{" "}
+    <div
+      className="App"
+      style={{
+        backgroundColor: theme.backgroundColor,
+        color: theme.color,
+        height: "100vh",
+      }}
+    >
+      <button
+        type="button"
+        onClick={toggle}
+        style={{
+          backgroundColor: theme.backgroundColor,
+          color: theme.color,
+          outline: "none",
+          boxShadow: `4px 4px 4px 4px ${theme.color}`,
+          marginTop: "20px",
+        }}
+        data-testid="toggle-theme-btn"
+      >
+        Toggle to {!dark ? "Dark" : "Light"} theme
+      </button>
+      <h1> Hangman</h1>
       {/* Maintain a state for correct letters, wrong letters, remaining letters. For this, the Letters component and the Hangman component as well as the Guessing component should be imported.
       Wrong letters is passed as a prop to HangmanFigure Component, correct letters passed to Guessing. */}{" "}
     </div>
