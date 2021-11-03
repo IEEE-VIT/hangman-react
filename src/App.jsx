7114e4a77c87077c0b82e591e71f57b3b14afc2a
Aneesh Panda <aneeshpanda.ap@gmail.com>
2021-11-04 03:50:04 +0530

    add: Home Page Layout (#40)

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,15 @@ import Hint from "./components/hint/hint.component";
 import { getRemainingLetters } from "./utils/helpers";
 import Letters from "./components/letters/Letters.component";
 import Guessing from "./components/guessing/Guessing.component";
-
-const getRandomWord = () => {
-  return randomWords();
-};
+import HangmanFigure from "./components/hangmanFigure/HangmanFigure.component";
 
 const App = () => {
+  function getRandomWord() {
+    if (randomWords().length > 8) {
+      return randomWords();
+    }
+    return getRandomWord();
+  }
   const [currentWord, setCurrentWord] = React.useState("");
   const { theme, toggle, dark } = React.useContext(ThemeContext);
   const [correctLetters, setCorrectLetters] = React.useState("");
@@ -21,9 +24,8 @@ const App = () => {
   const [showHint, setShowHint] = React.useState(false);
   const [showHintButton, setShowHintButton] = React.useState(true);
   const chances = wrongLetters.length < 6 ? 6 - wrongLetters.length : 0;
-  console.log(chances, wrongLetters.length);
+
   const handleClickTop = (letter) => {
-    console.log("clicked", letter);
     if (currentWord.includes(letter) === true) {
       setCorrectLetters(correctLetters + letter);
     } else if (!wrongLetters.includes(letter)) {
@@ -60,7 +62,21 @@ const App = () => {
       >
         Toggle to {!dark ? "Dark" : "Light"} theme
       </button>
-      <h1> Hangman</h1>
+      <h1> Hangman</h1> <br />
+      {showHint && <h3 className="hint">{hint.toUpperCase()}</h3>}
+      <HangmanFigure stroke={theme.color} wrongLetters={wrongLetters} />
+      {chances !== 0 && remainingLetters.length !== 0 && (
+        <>
+          {" "}
+          <Guessing
+            currentWord={currentWord}
+            remainingLetters={remainingLetters}
+            correctLetters={correctLetters}
+          />
+          <Letters handleClickTop={handleClickTop} word={currentWord} />
+          <div>Chances left: {chances}</div>
+        </>
+      )}
       {/* Maintain a state for correct letters, wrong letters, remaining letters. For this, the Letters component and the Hangman component as well as the Guessing component should be imported.
       Wrong letters is passed as a prop to HangmanFigure Component, correct letters passed to Guessing. */}{" "}
       {wrongLetters.length === 5 && showHintButton && (
@@ -82,14 +98,35 @@ const App = () => {
           Hint
         </button>
       )}
-      {showHint && <h3 className="hint">{hint.toUpperCase()}</h3>}
-      <Guessing
-        currentWord={currentWord}
-        remainingLetters={remainingLetters}
-        correctLetters={correctLetters}
-      />
-      <Letters handleClickTop={handleClickTop} word={currentWord} />
-      <div>Chances left: {chances}</div>
+      {chances === 0 ||
+        (remainingLetters.length === 0 && (
+          <>
+            <div>
+              {remainingLetters.length === 0
+                ? `You guessed it correctly, the word was ${currentWord}`
+                : `Correct Word was: ${currentWord}`}
+            </div>
+            <button
+              type="button"
+              style={{
+                backgroundColor: theme.backgroundColor,
+                color: theme.color,
+                outline: "none",
+                marginTop: "20px",
+              }}
+              onClick={() => {
+                setCurrentWord(getRandomWord());
+                setCorrectLetters("");
+                setWrongLetters("");
+                setRemainingLetters([]);
+                setShowHint(false);
+                setShowHintButton(true);
+              }}
+            >
+              Play Again!
+            </button>
+          </>
+        ))}
     </div>
   );
 };
