c1c96e5be4d43c7566dbe642e11aeebe8a86ac9e
Akshaya-vc <akshayav7c@gmail.com>
2021-11-03 17:07:46 +0530

    add: letters and letter component

--- src/components/letters/Letters.component.jsx
+++ src/components/letters/Letters.component.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import Letter from "../letter/Letter.component";
+import "./letters.styles.css";
 
 const Letters = () => {
+  //get 26 alphabets from alphabet function in helpers 
+  const letters = ["a", "b", "c", "a"];
+  const lettersList = letters.map((letter) => <Letter letterValue={letter} />);
   return (
-    <div>
-      {/*Get the letters of the english alphabet from the helpers. Pass each alphabet to the letter component with the help of a map function.*/}
+    <div className="letters">
+      {/* Get the letters of the english alphabet from the helpers. Pass each alphabet to the letter component with the help of a map function. */}
+      {lettersList}
     </div>
   );
 };
