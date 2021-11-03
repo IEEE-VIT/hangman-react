a7c42ac91d201e91b6ffe7456f792f92cbff16a9
Aneesh Panda <aneeshpanda.ap@gmail.com>
2021-11-04 01:02:47 +0530

    add: clicked letter status

--- src/components/guessing/Guessing.component.jsx
+++ src/components/guessing/Guessing.component.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Guessing = () => {
   return (
     <div>
-      {/*Display only the correct letters that are clicked. The Correct Letters will be passed from App.jsx.*/}
+      {/* Display only the correct letters that are clicked. The Correct Letters will be passed from App.jsx. */}
     </div>
   );
 };
--- src/components/letter/Letter.component.jsx
+++ src/components/letter/Letter.component.jsx
@@ -1,16 +1,27 @@
 import React, { useState } from "react";
 import "./letter.styles.css";
 
-const Letter = ({ letterValue, handleClick }) => {
+const Letter = ({ letterValue, handleClick, isCorrect }) => {
   const [clicked, setClicked] = useState(false);
 
   const handleonClick = () => {
     setClicked(true);
     handleClick(letterValue);
   };
+  let backgroundColor;
+  if (clicked && isCorrect) {
+    backgroundColor = "green";
+  } else if (clicked && !isCorrect) {
+    backgroundColor = "red";
+  }
   console.log(clicked);
   return (
-    <button className="letter" type="button" onClick={handleonClick}>
+    <button
+      className="letter"
+      type="button"
+      onClick={handleonClick}
+      style={{ backgroundColor }}
+    >
       {letterValue}
     </button>
   );
